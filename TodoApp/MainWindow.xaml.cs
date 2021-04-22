9c4cdac12db28fe27066fc85b29d6c495b90a24a
Luminar Light <18116946+LuminarLight@users.noreply.github.com>
2021-04-22 16:19:29 +0200

    haladás

--- TodoApp/MainWindow.xaml.cs
+++ TodoApp/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 
 namespace TodoApp
@@ -51,6 +52,60 @@ namespace TodoApp
             BindingOperations.SetBinding(descTextBox, TextBox.TextProperty, new Binding() { Source = g.DataContext, Path = new PropertyPath("Description"), Mode = BindingMode.TwoWay, UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged });
             BindingOperations.SetBinding(dueDatePicker, DatePicker.SelectedDateProperty, new Binding() { Source = g.DataContext, Path = new PropertyPath("DueDate"), Mode = BindingMode.TwoWay, UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged });
             BindingOperations.SetBinding(statusComboBox, ComboBox.SelectedItemProperty, new Binding() { Source = g.DataContext, Path = new PropertyPath("Status"), Mode = BindingMode.TwoWay, UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged });
+        }
+
+        private void GroupGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Grid g = sender as Grid;
+        }
+
+        private void ButtonNewTask_Click(object sender, RoutedEventArgs e)
+        {
+            int i = groupList.SelectedIndex;
+            if (i == -1)
+            {
+                MessageBox.Show("Please select a task group.");
+                return;
+            }
+            groups[i].Tasks.Add(new Task() { Title = "newtask", Description = "newdesc", DueDate = new DateTime(2025, 2, 1), Status = TaskStatus.Pending });
+        }
+
+        private void ButtonNewTaskGroup_Click(object sender, RoutedEventArgs e)
+        {
+            groups.Add(new TaskGroup() { Title = "newgroup" });
+        }
+
+        private void ButtonDeleteGroup_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to delete this group? This will also delete all the tasks in it.", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No);
+            if (result == MessageBoxResult.No) return;
+
+            var parent = VisualTreeHelper.GetParent(sender as Button) as UIElement;
+            Grid g = parent as Grid;
+            //MessageBox.Show(g.DataContext.GetType().GetProperty("Title").GetValue(g.DataContext).ToString());
+            TaskGroup t = g.DataContext as TaskGroup;
+            groups.Remove(t);
+        }
+
+        private void ButtonDeleteTask_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to delete this task?", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No);
+            if (result == MessageBoxResult.No) return;
+
+            var parent = VisualTreeHelper.GetParent(sender as Button) as UIElement;
+            Grid g = parent as Grid;
+            //MessageBox.Show(g.DataContext.GetType().GetProperty("Title").GetValue(g.DataContext).ToString());
+            Task t = g.DataContext as Task;
+            foreach (var group in groups)
+            {
+                if (group.Tasks.Remove(t))
+                {
+                    return;
+                }               
+            }
+
+            // Should not happen.
+            MessageBox.Show("Failed to delete task.");
         }
     }
 
