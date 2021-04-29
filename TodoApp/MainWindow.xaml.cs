5f78ca054e341ef57f3400ec8401d7ff884a1872
Luminar Light <18116946+LuminarLight@users.noreply.github.com>
2021-04-29 19:03:31 +0200

    haladás

--- TodoApp/MainWindow.xaml.cs
+++ TodoApp/MainWindow.xaml.cs
@@ -71,22 +71,12 @@ namespace TodoApp
 
         private void ButtonNewTask_Click(object sender, RoutedEventArgs e)
         {
-            int i = groupList.SelectedIndex;
-            if (i == -1)
-            {
-                if (groups.Count == 0)
-                {
-                    groups.Add(new TaskGroup() { Title = "newgroup" });
-                    groupList.SelectedIndex = 0;
-                    i = 0;
-                }
-                else
-                {
-                    MessageBox.Show("Please select a task group.");
-                    return;
-                }
-            }
-            groups[i].Tasks.Add(new Task() { Title = "newtask", Description = "", DueDate = DateTime.Now, Status = TaskStatus.Pending });
+            var parent = VisualTreeHelper.GetParent(sender as Button) as UIElement;
+            var parent2 = VisualTreeHelper.GetParent(parent) as UIElement;
+            Grid g = parent2 as Grid;
+            TaskGroup t = g.DataContext as TaskGroup;
+
+            t.Tasks.Add(new Task() { Title = "newtask", Description = "", DueDate = DateTime.Now, Status = TaskStatus.Pending });
         }
 
         private void ButtonNewTaskGroup_Click(object sender, RoutedEventArgs e)
@@ -96,14 +86,18 @@ namespace TodoApp
 
         private void ButtonDeleteGroup_Click(object sender, RoutedEventArgs e)
         {
-            MessageBoxResult result = MessageBox.Show("Are you sure you want to delete this group? This will also delete all the tasks in it.", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No);
-            if (result == MessageBoxResult.No) return;
-
             var parent = VisualTreeHelper.GetParent(sender as Button) as UIElement;
             var parent2 = VisualTreeHelper.GetParent(parent) as UIElement;
             Grid g = parent2 as Grid;
             //MessageBox.Show(g.DataContext.GetType().GetProperty("Title").GetValue(g.DataContext).ToString());
             TaskGroup t = g.DataContext as TaskGroup;
+
+            if (t.Tasks.Count > 0)
+            {
+                MessageBoxResult result = MessageBox.Show("Are you sure you want to delete this group? This will also delete all the tasks in it.", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No);
+                if (result == MessageBoxResult.No) return;
+            }            
+
             groups.Remove(t);
         }
 
@@ -287,6 +281,12 @@ namespace TodoApp
 
         private void MenuNew_Click(object sender, RoutedEventArgs e)
         {
+            if (groups.Count > 0)
+            {
+                MessageBoxResult result = MessageBox.Show("You are about to delete all groups and tasks. This action is irreversible. Are you sure you want to do this?", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No);
+                if (result == MessageBoxResult.No) return;
+            }
+            
             groups.Clear();
 
             lastPath = "";
