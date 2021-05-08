b61508f3cb9f88415ba8dec87f680047a70c1e16
Luminar Light <18116946+LuminarLight@users.noreply.github.com>
2021-05-08 13:45:40 +0200

    Removed unused usings.

--- TodoApp/MainWindow.xaml.cs
+++ TodoApp/MainWindow.xaml.cs
@@ -1,27 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-//using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using Microsoft.Win32;
-using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
-using System.Globalization;
 using System.ComponentModel;
 using System.Windows.Controls.Primitives;
-using System.Windows.Media.Animation;
 using TodoApp.Classes;
 
 namespace TodoApp
